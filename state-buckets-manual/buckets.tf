65bff3c0de15a82943bed15b20e22b06069657d6
bootaeva <bootaeva@gmail.com>
2026-02-16 22:59:59 -0800

    terraform fmt + change irsa arn

--- state-buckets-manual/buckets.tf
+++ state-buckets-manual/buckets.tf
@@ -14,12 +14,12 @@ resource "aws_s3_bucket" "statebucket" {
 }
 
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "terraformlock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "terraformlock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
   attribute {
-      name = "LockID"
-      type = "S"
+    name = "LockID"
+    type = "S"
   }
 
   tags = {
